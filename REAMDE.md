5eb6df0d07c2ddf7e91f981cafe3745db2fe47c8
LemurDev <leapinglemurjr@gmail.com>
2023-02-17 23:45:09 -0500

    First Commit

---
+++ REAMDE.md
@@ -0,0 +1,11 @@
+# Cozybot
+
+
+
+## TODO:
+Cozy meter visualization(sliding bar and once it gets to 100% (or 0%) some event happens)
+Something should happen if a user gets 100% cozy
+Cozy prayer makes cozyness precent higher via odds
+Cooldown for cozy command
+Progress bar represents the percentage of cozyness reached
+Sometime of Multiplier?
---
+++ src/main.py
@@ -0,0 +1,34 @@
+from twitchio.ext import commands
+import os
+from dotenv import load_dotenv
+import random
+
+load_dotenv()
+
+
+class Bot(commands.Bot):
+
+    old_cozyness = 0
+
+    def __init__(self):
+        super().__init__(token=os.environ.get("TOKEN"),
+                         prefix='?', initial_channels=[os.environ.get("CHANNEL_NAME")])
+
+    async def event_ready(self):
+        print(f'Logged in as | {self.nick}')
+
+    @commands.command()
+    async def cozy(self, ctx: commands.Context):
+        cozyness = str(random.randint(0, 100))
+        user = ctx.author.name
+        await ctx.send(f"@{ctx.author.name} is {cozyness}% cozy!")
+
+        if int(cozyness) > int(self.old_cozyness):
+            file = open("data/cozy.txt", "w")
+            file.write(f"{user} - {cozyness}%")
+            file.close()
+            self.old_cozyness = cozyness
+
+
+bot = Bot()
+bot.run()
